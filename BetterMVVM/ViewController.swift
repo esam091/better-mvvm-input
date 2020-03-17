e56f2e5f948c38b98b26f1b91320035386274fca
Samuel Edwin <esam091@gmail.com>
2020-03-17 07:57:03 +0700

    Initial Commit

---
+++ BetterMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterMVVM
+//
+//  Created by Samuel Edwin on 17/03/20.
+//  Copyright © 2020 Samuel Edwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
