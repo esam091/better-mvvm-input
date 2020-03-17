d1a3694f60aeef5be1097689f06862e0424c118f
Samuel Edwin <esam091@gmail.com>
2020-03-17 09:58:35 +0700

    plain vm done

--- BetterMVVM/ViewController.swift
+++ BetterMVVM/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RxCocoa
+import RxSwift
 
 class ViewModel {
     struct Input {
@@ -22,19 +23,70 @@ class ViewModel {
     }
     
     func transform(_ input: Input) -> Output {
+        let credentials = Driver.combineLatest(input.userName, input.password)
+        
+        let submission = input.submit.withLatestFrom(credentials)
+            
+        let error = submission.filter { (credential) -> Bool in
+            let (userName, password) = credential
+            
+            return userName.isEmpty || password.isEmpty
+        }.map { _ in
+            "Username and password must not be empty"
+        }
+        
+        let success = submission.filter { (credential) -> Bool in
+            let (userName, password) = credential
+            
+            return !userName.isEmpty && !password.isEmpty
+        }.map { _ in
+            
+        }
         
         return Output(
-            errorMessage: .empty(),
-            loginSuccess: .empty()
+            errorMessage: error,
+            loginSuccess: success
         )
     }
 }
 
 class ViewController: UIViewController {
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let input = ViewModel.Input(
+            userName: usernameTextField.rx.text.orEmpty.asDriver(),
+            password: passwordTextField.rx.text.orEmpty.asDriver(),
+            submit: loginButton.rx.tap.asDriver()
+        )
+        
+        let vm = ViewModel()
+        let output = vm.transform(input)
+        
+        output.loginSuccess.drive(onNext: { _ in
+            let alert = UIAlertController(title: "Done", message: "Done", preferredStyle: .alert)
+                
+                alert
+                    .addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }).disposed(by: disposeBag)
+        
+        output.errorMessage.drive(onNext: { message in
+            let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+                
+                alert
+                    .addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true, completion: nil)
+        }).disposed(by: disposeBag)
     }
 
 
